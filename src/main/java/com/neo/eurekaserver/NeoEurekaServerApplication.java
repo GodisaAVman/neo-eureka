ae0ab6eb57f5e0d63b4cbf6a0f374b6be760e775
GodisaAVman <570597738@qq.com>
2019-05-16 21:52:02 +0800

    init eureka server

---
+++ src/main/java/com/neo/eurekaserver/NeoEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.neo.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class NeoEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoEurekaServerApplication.class, args);
+	}
+
+}
